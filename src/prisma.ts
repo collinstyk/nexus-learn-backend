3367a884df9718d61064195f02e478433d64f96d
mattycollins803@gmail.com <mattycollins803@gmail.com>
2026-06-15 16:48:00 +0100

    fix prisma db connection issue

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "./generated/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
+import pg from  "pg";
 import  dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new pg.Pool();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({adapter});
 
